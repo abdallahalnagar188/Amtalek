dcc8918c3817f06c44e48a9c36977053a3718bd2
Mohamed Alaa <mohamedalaa4j@gmail.com>
2023-12-04 11:08:36 +0200

    working on advanced search design

--- app/src/main/java/eramo/amtalek/presentation/ui/main/market/SearchPropertyFragment.kt
+++ app/src/main/java/eramo/amtalek/presentation/ui/main/market/SearchPropertyFragment.kt
@@ -2,7 +2,10 @@ package eramo.amtalek.presentation.ui.main.market
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.WindowManager
+import android.widget.AdapterView
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -10,6 +13,10 @@ import androidx.viewbinding.ViewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import eramo.amtalek.R
 import eramo.amtalek.databinding.FragmentSearchPropertyBinding
+import eramo.amtalek.domain.model.auth.CityModel
+import eramo.amtalek.domain.model.auth.CountryModel
+import eramo.amtalek.presentation.adapters.spinner.CitiesSpinnerAdapter
+import eramo.amtalek.presentation.adapters.spinner.CountriesSpinnerAdapter
 import eramo.amtalek.presentation.ui.BindingFragment
 import eramo.amtalek.presentation.viewmodel.SharedViewModel
 import eramo.amtalek.presentation.viewmodel.navbottom.extension.NotificationViewModel
@@ -24,33 +31,92 @@ class SearchPropertyFragment : BindingFragment<FragmentSearchPropertyBinding>() 
     private val viewModel by viewModels<NotificationViewModel>()
     private val viewModelShared by activityViewModels<SharedViewModel>()
 
+    private var selectedCountryId = -1
+    private var selectedCityId = -1
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        super.registerApiRequest { viewModel.getNotification() }
-//        super.registerApiCancellation { viewModel.cancelRequest() }
-        StatusBarUtil.whiteWithBackground(requireActivity(), R.color.amtalek_blue_dark)
+
         setupToolbar()
+        setupViews()
+    }
 
-        binding.apply {
-            inSpinnerLocation.tvText.hint = getString(R.string.location)
-            inSpinnerType.tvText.hint = getString(R.string.property_type)
-            inSpinnerStatus.tvText.hint = getString(R.string.property_status)
-            inSpinnerCurrency.tvText.hint = getString(R.string.currency)
-            inSpinnerBathroom.tvText.hint = getString(R.string.min_bathrooms)
-            inSpinnerBeds.tvText.hint = getString(R.string.min_beds)
-
-            tvSearch.setOnClickListener {
-                findNavController().navigate(R.id.searchPropertyResultFragment)
-            }
-        }
+    private fun setupViews() {
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        StatusBarUtil.blackWithBackground(requireActivity(), R.color.white)
     }
 
     private fun setupToolbar() {
         binding.inToolbar.apply {
-//            ivSearch.isVisible = false
-            toolbarIvMenu.setOnClickListener { viewModelShared.openDrawer.value = true }
-            inNotification.root.setOnClickListener {
-                findNavController().navigate(R.id.notificationFragment)
+            ivClose.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    // -------------------------------------- setupViews -------------------------------------- //
+    private fun setupCountriesSpinner(data: List<CountryModel>) {
+        binding.apply {
+            val countriesSpinnerAdapter = CountriesSpinnerAdapter(requireContext(), data)
+            spinnerCountry.adapter = countriesSpinnerAdapter
+
+            spinnerCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val model = parent?.getItemAtPosition(position) as CountryModel
+                    selectedCountryId = model.id
+
+//                    viewModel.getCities(model.id.toString())
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
+
+            // refresh onClick if getting list fail
+            spinnerCountry.setOnTouchListener { view, motionEvent ->
+                when (motionEvent?.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        view.performClick()
+                        if (data.isEmpty()) {
+//                            viewModel.getCountries()
+                        }
+                    }
+                }
+                return@setOnTouchListener true
+            }
+        }
+    }
+
+    private fun setupCitiesSpinner(data: List<CityModel>) {
+        binding.apply {
+            val citiesSpinnerAdapter = CitiesSpinnerAdapter(requireContext(), data)
+            spinnerCity.adapter = citiesSpinnerAdapter
+
+            spinnerCity.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val model = parent?.getItemAtPosition(position) as CityModel
+
+                    selectedCityId = model.id
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
+
+            // refresh onClick if getting list fail
+            spinnerCity.setOnTouchListener { view, motionEvent ->
+                when (motionEvent?.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        view.performClick()
+                        if (data.isEmpty()) {
+//                            viewModel.getCountries()
+                        }
+                    }
+                }
+                return@setOnTouchListener true
             }
         }
     }
