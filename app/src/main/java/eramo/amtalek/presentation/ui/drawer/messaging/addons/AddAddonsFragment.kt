7d35ee89197bcc230c9ab64b8dba5381268182d1
Abdallah Alnajar <140744532+abdallahalnagar188@users.noreply.github.com>
2024-09-17 11:14:32 +0300

    update search form v3

--- app/src/main/java/eramo/amtalek/presentation/ui/drawer/messaging/addons/AddAddonsFragment.kt
+++ app/src/main/java/eramo/amtalek/presentation/ui/drawer/messaging/addons/AddAddonsFragment.kt
@@ -87,7 +87,6 @@ class AddAddonsFragment : BindingFragment<FragmentAddAdomsBinding>() {
                                             LoadingDialog.dismissDialog()
                                         }
                                     }
-
                                 }
                             }
                         }
